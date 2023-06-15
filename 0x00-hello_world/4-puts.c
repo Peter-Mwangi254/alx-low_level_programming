a6a5b2faa59ada41911cf1a278cbfac99034fcb3
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-15 02:09:03 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - Prints the main function
+ * Return - 0 if the programs runs successfully
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
