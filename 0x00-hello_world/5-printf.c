ecbf7539d1a44e68db0739e49cb59efea6b0c28c
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-15 02:22:36 -0700

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Description: main - Prints out the main function
+ * Return: returns 0 if program is successful
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+	return (0);
+}
