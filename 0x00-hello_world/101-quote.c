da1eb4ea31742edb23718afadac80c47c0df64a1
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-15 05:24:23 -0700

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Description: main- Contains the main function
- * Return: Returns 0 when program runs successfully
+ * Description: main- Prints the main function
+ * Return: Returns 0 when program is successful
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
- 
