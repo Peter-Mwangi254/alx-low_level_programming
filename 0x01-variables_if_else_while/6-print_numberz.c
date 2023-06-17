f8d8c2e7fa7c7f1a81813e5c0045cbf66730fe13
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-16 23:59:36 -0700

    Print Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Contains the main function
+ *
+ * Return: 0 when program runs successfully
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
