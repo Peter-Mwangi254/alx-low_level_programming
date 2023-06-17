157566633f928e6929baf7f0001787dbd490f256
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-16 21:55:19 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Contains the main function
+ *
+ * Return: 0 when program runs successfully
+ */
+
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
