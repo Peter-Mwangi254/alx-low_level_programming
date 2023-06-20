e21472b9796a5d04a80501d448051d6d0e535a36
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-20 05:22:09 -0700

    Identifies Alphabetic characters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,14 @@
 /**
  * _isalpha - Checks for alphabetic characters
  * @c : Character to be checked
- * Return : returns 1 if its an alphabetic character and 0 for anything else
+ * Return: returns 1 if its an alphabetic character and 0 for anything else
  */
 
- int _isalpha(int c)
+int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
-	{
-		return (1);
-	}
-	return (0);
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
 }
