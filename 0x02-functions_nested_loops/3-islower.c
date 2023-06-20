8b9ef65ade01cadde9256331722bba8ccccb3f96
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-20 04:52:49 -0700

    Identifies lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Checks for lowercase character
+ * @c: - Character been checked
+ * Return: - 1 if c is lowecase or 0 for else
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 12)
+	{
+
+		return (1);
+	}
+		else
+		return (0);
+}
