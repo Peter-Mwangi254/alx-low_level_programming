4058a446cae055b2d7dc51803c02bbbc5e71f122
Peter-Mwangi254 <pm6093628@gmail.com>
2023-06-23 13:11:11 -0700

    Prints Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,14 @@ void print_diagonal(int n)
 int i, j;
 
 if (n <= 0)
-	_putchar('\n');
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
-		_putchar('\n');
-	}
-
+_putchar('\n');
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
 }
