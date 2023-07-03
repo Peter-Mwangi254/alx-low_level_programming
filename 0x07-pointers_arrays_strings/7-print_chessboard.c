c23a22740eb8aecfd3341d43661b18b949211f87
Peter-Mwangi254 <pm6093628@gmail.com>
2023-07-03 14:48:40 -0700

    prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard 
- * @a: pointer to an 8x8 array of characters
- * Return: void
- */
+*print_chessboard - a function that prints the chessboard
+* @a: pointer to an 8x8 array of characters
+* Return: returns nothing
+*/
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
