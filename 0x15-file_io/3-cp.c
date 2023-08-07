5ac4453551527194be289fe7cbb55940ea936204
Peter-Mwangi254 <pm6093628@gmail.com>
2023-08-07 16:04:03 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,96 +1,68 @@
-#include "main.h"
-#define BUF_SIZE 1024
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include"main.h"
 
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
-* main - main function
-* @argc : number of arguments
-* @argv : a pointer to the array of arguments
-* Return:0 when successful
-**/
-
-int main(int argc, char **argv)
+ * main - copies the content of one file to another
+ * @argc: argument count
+ * @argv: arguments passed
+ *
+ * Return: 1 on success, exit otherwise
+ */
+int main(int argc, char *argv[])
 {
-int a0, a1, result1, result2;
-char *buffer;
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-buffer = malloc(sizeof(char) * BUF_SIZE);
-if (!buffer)
-return (0);
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
 
-a1 = open(argv[1], O_RDONLY);
-error_98(a1, buffer, argv[1]);
-a0 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-error_99(a0, buffer, argv[2]);
-do {
-	result1 = read(a1, buffer, BUF_SIZE);
-	if (result1 == 0)
-		break;
-	error_98(result1, buffer, argv[1]);
-	result2 = write(a0, buffer, result1);
-	error_98(result2, buffer, argv[2]);
-	} while (result2 >= BUF_SIZE);
-	result1 = close(a0);
-	error_100(result1, buffer);
-	free(buffer);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	src = open(argv[1], O_RDONLY);
+	check_IO_stat(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
+	{
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, argv[1], 'O');
+		wrote = write(dest, buffer, n_read);
+		if (wrote == -1)
+			check_IO_stat(-1, -1, argv[2], 'W');
+	}
+	close_src = close(src);
+	check_IO_stat(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest, NULL, 'C');
 	return (0);
 }
 
 /**
-* error_98 - checks the error 98
-* @a0: value to check
-*@buffer: the buffer
-*@argv: argument vector
-**/
-
-void error_98(int a0, char *buffer, char *argv)
+ * check_IO_stat - checks if a file can be opened or closed
+ * @stat: file descriptor of the file to be opened
+ * @filename: name of the file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check_IO_stat(int stat, int fd, char *filename, char mode)
 {
-if (a0 < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", a0);
-free(buffer);
-exit(98);
-}
-}
-
-/**
-* error_99 - checks error 99
-* @buffer: the buffer
-* @a0: value to check
-* @argv: argument
-*/
-
-void error_99(int a0, char *buffer, char *argv)
-{
-if (a0 < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
-free(buffer);
-exit(99);
-}
-}
-
-/**
-* error_100 - checks the error 100
-* @a0: the value to check
-* @buffer: the buffer
-**/
-
-void error_100(int a0, char *buffer)
-{
-if (a0 < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", a0);
-free(buffer);
-exit(100);
-}
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	else if (mode == 'W' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
