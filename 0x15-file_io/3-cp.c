7163f1faeee04ee7ccdde8396191ce58dcbac7d6
Peter-Mwangi254 <pm6093628@gmail.com>
2023-08-07 15:21:45 -0700

    program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#define BUF_SIZE 1024
+
+/**
+* main - main function
+* @argc : number of arguments
+* @argv : a pointer to the array of arguments
+* Return : 0 when succesful
+**/
+
+int main(int argc, char **argv)
+{
+int a0, a1, result1, result2;
+char *buffer;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+buffer = malloc(sizeof(char) * BUF_SIZE);
+if (!buffer)
+return (0);
+
+a1 = open(argv[1], O_RDONLY);
+error_98(a1, buffer, argv[1]);
+a0 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+error_99(a0, buffer,argv[2]);
+do{
+	result1 = read(a1, bugger,BUF_SIZE);
+	if (result1 == 0)
+		break;
+	error_98(result1, buffer, argv[1];
+	result2 = write(a0, buffer, result1);
+	error_98(result2, buffer, argv[2];
+	} while (result2 >= BUF_SIZE);
+	result1 = close(a0);
+	error_100(result1, buffer);
+	free(buffer);
+	return (0);
