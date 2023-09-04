cb87b574d3b37f697e80da1fc914b01a46913ac6
Peter-Mwangi254 <pm6093628@gmail.com>
2023-09-04 15:14:45 -0700

    Changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,34 @@
 #include "main.h"
-
 /**
- * create_file - create and write into a file
- * and copies content in it
- * @filename : the file to be created
- * @text_content : content to be copied ino the file
- * Return: 1 successful or -1 if failed
- */
-
-iint create_file(const char *filename, char *text_content)
+  *create_file - creates the file
+  *@filename: the pointer to the file
+  *@text_content: text content
+  *Return: -1 (error/ failed) or 1 (success)
+  */
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t bytesW;
-	int fd;
+	int fd, len = 0, w;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 	{
 		return (-1);
 	}
-	if (text_content)
-	{
-		bytesW = write(fd, text_content, strlen(text_content));
-		if (bytesW == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-
 	close(fd);
+
 	return (1);
 }
-}
-
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,35 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename : the file to append text on
- * @text_content : NULL terminated string to add at the end of the file
- * Return: 1 on success and -1 on failure
- */
-
+  *append_text_to_file - appends text to the the end of the file
+  *@filename: the file
+  *@text_content: the text to append
+  *Return: 1(success) or -1(failure);
+  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytesW;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 	{
 		return (-1);
 	}
-	if (text_content != NULL && strlen(text_content) > 0)
-	{
-		bytesW = write(fd, text_content, strlen(text_content));
-		if (bytesW == -1)
-		{
-			return (-1);
-			close(fd);
-		}
-	}
+
 	close(fd);
+
 	return (1);
 }
