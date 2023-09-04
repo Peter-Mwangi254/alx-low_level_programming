2f86bd26923cefdf73af6b2e715d79fbb1faa0b5
Peter-Mwangi254 <pm6093628@gmail.com>
2023-09-04 15:08:19 -0700

    Fileio

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,8 @@
 #include<stdlib.h>
 #include<sys/types.h>
 #include<sys/stat.h>
-#include<fcntl.h>
-
+#include <fcntl.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
